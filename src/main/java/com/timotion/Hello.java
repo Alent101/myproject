062870fb17a86e4a354c41cf2b86b3ef42fdd83f
Alent <alent101@gmail.com>
2019-02-11 23:28:30 +0800

    Java and Kotlin varable, data type

--- src/main/java/com/timotion/Hello.java
+++ src/main/java/com/timotion/Hello.java
@@ -4,6 +4,13 @@ public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello world");
         Person p = new Person();
-        p.hello();;
+        p.hello();
+        int age = 2;
+        Integer age2 = 2;
+        age2.toString();
+        float w = 12.5f;
+        char c = 'A';
+        boolean adult = true;
+        String name = "Alent";
     }
 }
--- src/main/kotlin/com/timotion/Hello.kt
+++ src/main/kotlin/com/timotion/Hello.kt
@@ -3,8 +3,12 @@ package com.timotion
 fun main(args: Array<String>) {
 //    println("Hello world")
 //    Human().hello();
+    // command + / 快速註解
     var h = Human()
     h.hello()
+    // var 可改變值 val 不可改變值
+    var age = 19;
+    var name:String = "Alent";
 }
 
 class Human{
